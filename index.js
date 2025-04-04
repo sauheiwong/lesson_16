fb587aac0d640f85ab02facea6f91e3d1788aa39
sauheiwong <sauheiwong@gmail.com>
2025-04-03 17:43:38 -0700

    improve getGIF

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// https://api.giphy.com/v1/gifs/search?api_key=A7vih9kjPI2HKR3lV9LcW2jYDPEoLbrX&q=happy&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
-
 require("dotenv").config();
+
+const keyword = process.argv[2] ?? "happy";
+const API_KEY = process.env.API_KEY;
 
 async function getGIF() {
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=happy&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
   );
   const data = await response.json();
   console.log(data.data);
